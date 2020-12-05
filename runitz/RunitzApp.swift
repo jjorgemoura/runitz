98d716181ee82e7e5acac9d03a4b5840ceae3dc6
Jorge Moura <jjorge.moura@gmail.com>
2020-12-05 15:10:08 +0000

    Init store in HomeView

--- runitz/RunitzApp.swift
+++ runitz/RunitzApp.swift
@@ -2,12 +2,30 @@
 // Copyright © 2020  Jorge Moura. All rights reserved.
 
 import SwiftUI
+import ComposableArchitecture
 
 @main
 struct RunitzApp: App {
+
+    let store = Store(
+        initialState: initState(),
+        reducer: appReducer,
+        environment: initEnvironment()
+    )
+
     var body: some Scene {
         WindowGroup {
-            Home()
+            HomeView(store: store)
         }
+    }
+
+    // MARK: - private methods
+
+    private static func initState() -> AppState {
+        return AppState(id: UUID(), currentDistance: Distance(value: 5, unit: .km, isFavourite: false))
+    }
+
+    private static func initEnvironment() -> AppEnvironment {
+        return AppEnvironment()
     }
 }
